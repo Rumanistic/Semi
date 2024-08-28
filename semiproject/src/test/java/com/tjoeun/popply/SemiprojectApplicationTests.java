be29a4d2a57d0c5f99b3fdb3a9ef8f9eb2665184
rumanistic <xinatic@naver.com>
2024-08-28 17:15:48 +0900

    20240824_프로젝트시작

---
+++ semiproject/src/test/java/com/tjoeun/popply/SemiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjoeun.popply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
