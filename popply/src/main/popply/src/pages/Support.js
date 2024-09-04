6056cb1c29c7b93c231b0fc6081638dc6ebaad6c
rumanistic <xinatic@naver.com>
2024-09-04 15:06:52 +0900

    0904 supports 분리
    faq -> supports/faq
    support -> supports/usersupport
    으로 내용 분리

--- popply/src/main/popply/src/pages/Support.js
+++ popply/src/main/popply/src/pages/Support.js
@@ -1,14 +1,35 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Faq from './support/Faq';
+import UserSupport from './support/UserSupport';
+import * as SupportStyle from './styles/SupportStyle'
 
 function Support() {
 
+	const navigate = useNavigate();
+	
   return (
-    <div>
-    	<h1 align='center'>고객 지원</h1>
-    </div>
+    <SupportStyle.SupportContainer>
+    	<SupportStyle.AsideNavContainer>
+    		<SupportStyle.AsideNavMenuContainer>
+    			<ul>
+	    			<SupportStyle.NavMenuContent onClick={() => { navigate('./faq')}}>FAQ</SupportStyle.NavMenuContent>
+	    			<SupportStyle.NavMenuContent onClick={() => { navigate('./usersupport')}}>고객지원</SupportStyle.NavMenuContent>
+    			</ul>
+    		</SupportStyle.AsideNavMenuContainer>
+    	</SupportStyle.AsideNavContainer>
+    	<SupportStyle.SectionContainer>
+    		<article>
+    			<Routes>
+    				<Route path='/faq' element={<Faq />}/>
+    				<Route path='/usersupport' element={<UserSupport />}/>
+    			</Routes>
+    		</article>
+    	</SupportStyle.SectionContainer>
+    </SupportStyle.SupportContainer>
   );
 }
 
 
 export default Support;
+
+/* */
