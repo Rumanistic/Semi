aad81f2a11be5770d539ee6564b7af11b47b752d
ohayo <qkzkel17@naver.com>
2024-09-19 09:52:50 +0900

    2024.09.19

--- popply/src/main/popply/src/pages/Footer.js
+++ popply/src/main/popply/src/pages/Footer.js
@@ -1,59 +1,7 @@
 import React, { useState }from 'react';
 
 const Footer = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [author, setAuthor] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('제목:', title);
-        console.log('내용:', content);
-        console.log('작성자:', author);
-
-        // 양식 초기화
-        setTitle('');
-        setContent('');
-        setAuthor('');
-    };
-	
-	
-
-  return (
-    <div className="footer-container">
-            <h1>게시물 등록</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="title">제목:</label>
-                <input
-                    type="text"
-                    id="title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    required
-                />
-
-                <label htmlFor="content">내용:</label>
-                <textarea
-                    id="content"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    rows="5"
-                    required
-                ></textarea>
-
-                <label htmlFor="author">작성자:</label>
-                <input
-                    type="text"
-                    id="author"
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                    required
-                />
-
-                <button type="submit">등록</button>
-            </form>
-        </div>
-  );
 };
 
 
